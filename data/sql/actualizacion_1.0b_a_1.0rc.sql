4acafdc88c55f94ce1e0e964e21281edb19a2ec6
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-01-15 15:51:52 +0000

    Quitando datos que pasaron a datos_desde_cero.sql

--- data/sql/actualizacion_1.0b_a_1.0rc.sql
+++ data/sql/actualizacion_1.0b_a_1.0rc.sql
@@ -1,4 +1,2 @@
 ALTER TABLE `tipoiva` CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
 
-INSERT INTO `tipodocumento` ( `id` , `descripcion` , `orden` , `nombre` ) VALUES (NULL , 'Documento Nacional de Identidad', '0', 'DNI'), (NULL , 'Libreta de Enrolamiento', '0', 'LE');
-INSERT INTO `tipodocumento` ( `id` , `descripcion` , `orden` , `nombre` ) VALUES (NULL , 'Cedula de Identidad', '0', 'CI'), (NULL , 'Pasaporte', '0', 'Pasaporte');
