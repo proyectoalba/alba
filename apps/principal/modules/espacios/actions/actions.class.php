9937ec8d00b95073abf63cd17f31c9e7b0532c4c
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-01-29 19:04:24 +0000

    modificacion para subsanar error de caracter raro al copiar el preexecute

--- apps/principal/modules/espacios/actions/actions.class.php
+++ apps/principal/modules/espacios/actions/actions.class.php
@@ -23,8 +23,8 @@
  * espacios Acciones
  *
  * @package    alba
- * @author     José Luis Di Biase <josx@interorganic.com.ar>
- * @author     Héctor Sanchez <hsanchez@pressenter.com.ar>
+ * @author     JosÃ© Luis Di Biase <josx@interorganic.com.ar>
+ * @author     HÃ©ctor Sanchez <hsanchez@pressenter.com.ar>
  * @author     Fernando Toledo <ftoledo@pressenter.com.ar>
  * @version    SVN: $Id$
  * @filesource
@@ -34,7 +34,9 @@
 class espaciosActions extends autoespaciosActions
 {
      public function preExecute() {
-             $this->vista = $this->getRequestParameter('vista');
+         $this->vista = $this->getRequestParameter('vista');
      }
 
 }
+
+?>
