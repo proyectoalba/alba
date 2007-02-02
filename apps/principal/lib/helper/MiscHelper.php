b4d2189add5b6168312f44d4f796e957584ac928
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-02-02 15:09:18 +0000

    cambiando indice del array de mese

--- apps/principal/lib/helper/MiscHelper.php
+++ apps/principal/lib/helper/MiscHelper.php
@@ -55,7 +55,7 @@
     }
 
     function Meses() {
-        $aMeses = array( '01' => 'Enero', '02' => 'Febrero', '03' => 'Marzo', '04' => 'Abril', '05' => 'Mayo', '06' => 'Junio', '07' => 'Julio', '08' => 'Agosto', '09' => 'Septiembre', '10' => 'Octubre', '11' => 'Noviembre', '12' => 'Diciembre');
+        $aMeses = array( 1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril', 5 => 'Mayo', 6 => 'Junio', 7 => 'Julio', 8 => 'Agosto', 9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre');
         return  $aMeses;
     }    
  
