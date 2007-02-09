fb785cfe59437f651b7f00aaeafb26ca365ab888
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-02-09 21:05:39 +0000

    Agregue la Constancia de Alumno Regular

--- apps/principal/modules/informes/templates/constanciaAlumnoRegularListadoSuccess.php
+++ apps/principal/modules/informes/templates/constanciaAlumnoRegularListadoSuccess.php
@@ -1,14 +1,78 @@
-GOBIERNO DE LA CIUDAD DE BUENOS AIRES<br>
-SECRETAR&Iacute;A DE EDUCACI&Oacute;N<br>
-<?=$establecimiento->getNombre()?> <br>
-<? if($locacion) { ?><?=$locacion->getDireccion()?> - T&eacute;lefono: <?=$locacion->getTelefono()?> (<?=$locacion->getCodigoPostal()?>) <br> <?=$locacion->getCiudad()?><br><? } ?>
-CONSTANCIA DE ALUMNO REGULAR
-<br><br>
-Se hace constar de <?=$alumno->getApellido()?>, <?=$alumno->getNombre()?> es un alumno regular de:<br><br>
-A&ntilde;o: <?=$division->getAnio()->getDescripcion()?> Divisi&oacute;n: <?=$division->getDescripcion()?> Turno: <?=$turnos->getDescripcion();?>, en este establecimiento<br>
+<style>
 
-<!--
-Fecha:
-Presentar en:
-Firma:
--->
+.titulo
+{
+  color: #000;
+  font-family: "Trebuchet MS", Arial, Verdana, sans-serif;
+  font-size: 20px;
+  font-weight: bold
+}
+
+.subtitulo
+{
+  color: #000;
+  font-family: "Trebuchet MS", Arial, Verdana, sans-serif;
+  font-size: 18px;
+  font-weight: bold
+}
+
+.texto
+{
+  color: #000;
+  font-family: "Trebuchet MS", Arial, Verdana, sans-serif;
+  font-size: 17px;
+}
+
+</style>
+
+
+
+<div style="text-align: center;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tbody>
+    <tr>
+      <td><?=image_tag('small/escudo_bsas_chico.jpg')?></td>
+      <td>
+      <div style="text-align: left;"></div>
+      <div style="text-align: center;"><span class="titulo">GOBIERNO DE LA CIUDAD DE BUENOS AIRES</span><br>
+      <span class="titulo">SECRETAR&Iacute;A DE EDUCACI&Oacute;N</span><br>
+      <br>
+      </div>
+      <div style="text-align: center;" class="subtitulo"><?=strtoupper($establecimiento->getNombre())?> <br> 
+        <? if($locacion) { ?><?=$locacion->getDireccion()?> - T&eacute;lefono: <?=$locacion->getTelefono()?> (<?=$locacion->getCodigoPostal()?>) <?=$locacion->getCiudad()?><br><? } ?>
+      <br>
+      </div>
+      <div style="text-align: center;"><span class="titulo">CONSTANCIA DE ALUMNO REGULAR<br>
+      </div>
+      <span style="font-weight: bold;"></span></td>
+    </tr>
+      <tr>
+      <td colspan="2" rowspan="1">
+        <div style="text-align: left;" class="texto">
+        <br><br>Por medio de la presente se deja constancia que <?=$alumno->getApellido()?>, <?=$alumno->getNombre()?> es un alumno regular de:<br>
+        A&ntilde;o: <span style="font-weight: bold;"><?=$division->getAnio()->getDescripcion()?></span>, Divisi&oacute;n: <span style="font-weight: bold;"><?=$division->getDescripcion()?></span> , Turno: <span style="font-weight: bold;"><?=$turnos->getDescripcion();?></span>, en este establecimiento<br> Se emite este certificado para presentar ante quien corresponda.
+      <br>
+      <br>
+        <span style="font-weight: bold;">Lugar y Fecha:</span> Ciudad Aut&oacute;noma de Buenos Aires, <?php echo date("d/m/Y");?>      
+        </div>
+    </td>
+    
+   </tr>
+    <tr>
+      <td colspan="2" rowspan="1"><br>
+      <br><br><br>
+        <div style="text-align: right" class="texto" >Firma del Responsable del Establecimiento</div>
+      </td>
+    </tr>
+</tbody>
+</table>
+</div>
+
+
+
+
+
+
+
+
+
