828d1781c038d5c369eae8850a318c465d8d78ff
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-02-12 21:00:45 +0000

    Mejora a la grafica del boletin

--- apps/principal/modules/boletin/templates/mostrarSuccess.php
+++ apps/principal/modules/boletin/templates/mostrarSuccess.php
@@ -4,6 +4,16 @@
   <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
   <title>Boletine</title>
 </head>
+<style>
+.etiquetas
+{
+  color: #000;
+  font-family: "Trebuchet MS", Arial, Verdana, sans-serif;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
 <body>
 
 <?php if ($sf_flash->has('notice')) { ?>
@@ -23,48 +33,47 @@ TURNO <b><?=$division->getTurnos()->getDescripcion()?></b><br>
 <table style="text-align: left; width: 766px; height: 143px;" border="1" cellpadding="2" cellspacing="2">
   <tbody>
     <tr>
-      <td colspan="1" rowspan="2"><div class="verticaltext">PERIODO</div></td>
-      <td colspan="<?=$cantOptionsActividad?>" rowspan="1">AREAS FORMATIVAS</td>
-      <td colspan="<?=$cantOptionsConcepto?>" rowspan="1">APRECIACION PERSONAL</td>
-      <td colspan="2" rowspan="1">CONTROL DE ASISTENCIAS</td>
-      <td colspan="2" rowspan="1">FIRMAS</td>
+      <td colspan="1" rowspan="2"><div class="etiquetas">PERIODO</div></td>
+      <td colspan="<?=$cantOptionsActividad?>" rowspan="1"><div class="etiquetas">AREAS FORMATIVAS</div></td>
+      <td colspan="<?=$cantOptionsConcepto?>" rowspan="1"><div class="etiquetas">APRECIACION PERSONAL</div></td>
+      <td colspan="2" rowspan="1"><div class="etiquetas">CONTROL DE ASISTENCIAS</div></td>
+      <td colspan="3" rowspan="1"><div class="etiquetas">FIRMAS</div></td>
     </tr>
     <tr>
 
       <? foreach($optionsActividad as $actividad) { ?> 
-      <td><?=$actividad?></td>
+      <td><div class="etiquetas"><?=$actividad?></div></td>
       <? } ?>
 
       <? foreach($optionsConcepto as $concepto) { ?> 
-      <td><?=$concepto?></td>
+      <td><div class="etiquetas"><?=$concepto?></div></td>
       <? } ?>
 
       <? if(count($aAsistencia)>0) { 
             foreach( current($aAsistencia) as $grupo => $valor) { ?> 
-      <td><?=$grupo?></td>
+      <td><div class="etiquetas"><?=$grupo?></div></td>
       <?
             }
         } ?>
 
-      <td>Maestro</td>
-      <td>Director</td>
+      <td><div class="etiquetas">Maestro</div></td>
+      <td><div class="etiquetas">Director</div></td>
     </tr>
 
 <? foreach($optionsPeriodo as $periodo_id => $periodo) { ?>
     <tr>
-      <td><?=$periodo?></td>
+      <td><div class="etiquetas"><?=$periodo?></div></td>
     <? foreach($optionsActividad as $actividad_id => $actividad) { ?> 
-      <td><?=(array_key_exists($periodo_id, $notaAlumno) AND array_key_exists($actividad_id, $notaAlumno[$periodo_id]))?$notaAlumno[$periodo_id][$actividad_id]:"";?>
-    </td>
+      <td><div style="text-align: center;"><?=(array_key_exists($periodo_id, $notaAlumno) AND array_key_exists($actividad_id, $notaAlumno[$periodo_id]))?$notaAlumno[$periodo_id][$actividad_id]:"";?>
+    </div></td>
     <? } ?>      
     <? foreach($optionsConcepto as $concepto_id => $concepto) { ?> 
-      <td><?=(array_key_exists($periodo_id, $conceptoAlumno) AND array_key_exists($concepto_id, $conceptoAlumno[$periodo_id]))?$conceptoAlumno[$periodo_id][$concepto_id]:"";?>
+      <td><div style="text-align: center;"><?=(array_key_exists($periodo_id, $conceptoAlumno) AND array_key_exists($concepto_id, $conceptoAlumno[$periodo_id]))?$conceptoAlumno[$periodo_id][$concepto_id]:"";?></div></td>
     <? } ?>      
 
 <? if(count($aAsistencia)>0) { 
        foreach( $aAsistencia[$periodo_id] as $grupo => $valor) { ?> 
-      <td><?=$valor?>
-</td>
+      <td><div style="text-align: center;"><?=$valor?></div></td>
       <?
     }
         } ?>
