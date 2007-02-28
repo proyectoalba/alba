5dd9bc2ad4e204094b0ba13fc41f360697963635
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-02-28 21:57:49 +0000

    Arreglo para validar fk_locacion_id en espacio (ticket #167)

--- apps/principal/modules/horarioescolar/templates/_fk_turnos_id.php
+++ apps/principal/modules/horarioescolar/templates/_fk_turnos_id.php
@@ -1,1 +1,11 @@
-<? echo $horarioescolar->getTurnos()->getDescripcion();?>
+<?
+    $aTurnos = array();
+    $c = new Criteria();
+    $c->add(TurnosPeer::FK_CICLOLECTIVO_ID,$sf_user->getAttribute('fk_ciclolectivo_id'));
+    $sqlTurnos  = TurnosPeer::doSelect($c);
+    foreach($sqlTurnos as $turnos) {
+        $aTurnos[$turnos->getId()] = $turnos->getDescripcion();
+    }
+
+    echo select_tag("horarioescolar[fk_turnos_id]", options_for_select($aTurnos, $horarioescolar->getFkTurnosId()));
+?>
