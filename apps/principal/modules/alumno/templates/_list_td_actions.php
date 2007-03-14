516d5ef3b7c514a25a74fcfb9aa2b5a51fa32a5f
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-03-14 19:10:47 +0000

    Cambiando el link para ir a la cuenta

--- apps/principal/modules/alumno/templates/_list_td_actions.php
+++ apps/principal/modules/alumno/templates/_list_td_actions.php
@@ -9,7 +9,7 @@
   'post' => true,
   'confirm' => __('Are you sure?'),
 )) ?></li>
-  <li><?php echo link_to(image_tag('next.png', array('alt' => __('Ir a Cuenta'), 'title' => __('Ir a Cuenta'))), 'cuenta/verCompleta?id='.$alumno->getFkCuentaId()) ?></li>
+  <li><?php echo link_to(image_tag('small/ir_a.png', array('alt' => __('Ir a Cuenta'), 'title' => __('Ir a Cuenta'))), 'cuenta/verCompleta?id='.$alumno->getFkCuentaId()) ?></li>
   <li><?php echo link_to(image_tag('jeringa.gif', array('alt' => __('Vacunas'), 'title' => __('Vacunas'))), 'alumno/vacunas?id='.$alumno->getId()) ?></li>
   <li><?php echo link_to(image_tag('small/asistencia.png', array('alt' => __('Asistencia'), 'title' => __('Asistencia'))), 'alumno/asistencia?id='.$alumno->getId()) ?></li>
   <li><?php echo link_to(image_tag('small/legajo.png', array('alt' => __('Legajo'), 'title' => __('Legajo'))), 'alumno/legajo?id='.$alumno->getId()) ?></li>
