4a0aec4674644666cd3005f744567a3a2f6bb7f4
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-03-19 19:53:09 +0000

    Agregue ajax de pais/provincia para locacion y organizacion

---
+++ apps/principal/modules/organizacion/templates/cambiarPaisSuccess.php
@@ -0,0 +1,8 @@
+<?php
+    use_helper("Object");
+?>
+<div id="item_provincia">
+<?php 
+    echo select_tag('organizacion[fk_provincia_id]', objects_for_select($provincias, 'getId','getNombreLargo',$provincia_id), array('control_name' => 'organizacion[fk_provincia_id]'));
+?>
+</div>
