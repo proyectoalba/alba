981aa494f5633bb5245d520020cc71b88e5c62dd
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-03-19 16:15:25 +0000

    Agregando mensaje de error al eliminar alumno

--- apps/principal/modules/alumno/templates/_list_header.php
+++ apps/principal/modules/alumno/templates/_list_header.php
@@ -3,5 +3,13 @@
 if (isset($filters['division']) && $filters['division'] != '' && $filters['division'] != 0) {
     $division = DivisionPeer::retrieveByPk($filters['division']);
     ?> de <? echo $division->getAnio()->getDescripcion()." ".$division->getDescripcion();?>
-<? } ?>
+<? }?>
 </h1>
+<?php if ($sf_request->getError('delete')): ?>
+<div class="form-errors">
+  <h2>Could not delete the selected Alumno</h2>
+  <ul>
+    <li><?php echo $sf_request->getError('delete') ?></li>
+  </ul>
+</div>
+<?php endif; ?>
