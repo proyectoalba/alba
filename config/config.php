01a5725b562c478f49eda997f4d28f932a0751a8
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-07-30 14:58:43 +0000

    sacando links simbolicos para separa el proyecto del framework goodbye sandbox =)

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // symfony directories
-$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/symfony';
-$sf_symfony_data_dir = dirname(__FILE__).'/../data/symfony';
+$sf_symfony_lib_dir  = realpath(dirname(__FILE__).'/../../dist/symfony/lib');
+$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../dist/symfony/data');
