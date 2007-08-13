2f368c1d75f28f77680ba3888d426a7fa0236c98
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-08-13 19:21:05 +0000

    Sacando cosas que no se usan del layout de impresion

--- apps/principal/templates/layout_impresion.php
+++ apps/principal/templates/layout_impresion.php
@@ -6,36 +6,6 @@
     <?php echo include_title() ?>
     <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot() . '/js/jsmenu/themes/'.sfConfig::get("app_alba_menutheme").'/theme.css' ?>"/>
     <link rel="shortcut icon" href="/favicon.ico" />
-
-    <style type="text/css">
-    .fdown {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/index3_5x2.png"?>);
-    }
-    .fmedio {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/fmedio.png"?>);
-    }
-    .ftop {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/index3_1x4.png"?>);
-    }
-    .fmenu {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/fmenu.png"?>);
-    }
-    td {
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 11px;
-    }
-    .fizq {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/fizq.png"?>);
-    }
-    .fizq-menu {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/fizq-menu.png"?>);
-    }
-    .menuimg {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/index_2x1.png"?>);
-    }
-    .fder {
-        background-image: url(<?=sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/images/gui/fder.png"?>);
-    }
     </style>
         <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot()?>/css/impresion.css" />
     <?php if (!$sf_user->isAuthenticated()):?>
@@ -43,7 +13,6 @@
     <?php endif;?>
     </head>
     <body>
-<?php echo $content; ?>
+        <?php echo $content; ?>
     </body>
-<?php echo javascript_include_tag('varios/wz_tooltip.js'); ?>
     </html>
