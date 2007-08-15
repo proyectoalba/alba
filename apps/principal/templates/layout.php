11401dc4781c60ff531f8a3129ba2bdc16da3133
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-08-15 13:45:46 +0000

    Agregue un favicon para el sistema

--- apps/principal/templates/layout.php
+++ apps/principal/templates/layout.php
@@ -4,13 +4,13 @@
     <?php echo include_http_metas() ?>
     <?php echo include_metas() ?>
     <?php echo include_title() ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot() . '/js/jsmenu/themes/'.sfConfig::get("app_alba_menutheme").'/theme.css' ?>"/>
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot() . 'js/jsmenu/themes/'.sfConfig::get("app_alba_menutheme").'/theme.css' ?>"/>
+    <link rel="shortcut icon" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot() ?>favicon.ico" />
 
-    <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot()?>/css/marco_imagenes.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot()?>css/marco_imagenes.css">
 
     <?php if (!$sf_user->isAuthenticated()):?>
-        <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot()?>/css/login.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo sfContext::getInstance()->getRequest()->getRelativeUrlRoot()?>css/login.css">
     <?php endif;?>
     </head>
     <body>
