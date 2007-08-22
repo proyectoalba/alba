af843197be33f61476e57b5b3af2069965ec6ce4
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-08-22 15:05:32 +0000

    ignorando config.php.bak

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // symfony directories
-$sf_symfony_lib_dir  = realpath(dirname(__FILE__).'/../../dist/symfony/lib');
-$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../dist/symfony/data');
+$sf_symfony_lib_dir  = '/home/ftoledo/alba/dist/symfony-1.0.5/lib';
+$sf_symfony_data_dir = '/home/ftoledo/alba/dist/symfony-1.0.5/data';
