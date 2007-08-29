5d287c5334f8874caa3b1ac49873f3aff056c8d8
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-08-29 17:05:00 +0000

    Cambiando el titulo

--- apps/principal/modules/tipodocente/templates/_edit_header.php
+++ apps/principal/modules/tipodocente/templates/_edit_header.php
@@ -1,1 +1,1 @@
-<h1><?php echo ($sf_params->get('action') == "create")?"Ingresar Tipo de Docente":"Editar Tipo de Docente";?></h1>
+<h1><?php echo ($sf_params->get('action') == "create")?"Ingresar Tipo de Docente":"Modificar Tipo de Docente";?></h1>
