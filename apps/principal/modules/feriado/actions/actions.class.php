16034caaabe71428fea8fae1cdca79dc38bd29dd
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-09-05 15:13:22 +0000

    Mejora en la validacion de fechas en el feriado.

--- apps/principal/modules/feriado/actions/actions.class.php
+++ apps/principal/modules/feriado/actions/actions.class.php
@@ -43,53 +43,47 @@ class feriadoActions extends autoferiadoActions
     Sobreescribo la accion del generator porque nefecito que el combo 
     del ciclo lectivo sea dependiente de la session fk_establecimiento_id
 */
-  public function executeEdit ()                                                                    
-  {                                                                                                 
-    $this->feriado = $this->getFeriadoOrCreate();                    
-    
-    $criteria = new Criteria();                                                       
+  public function executeEdit ()
+  {
+    $this->feriado = $this->getFeriadoOrCreate();
+    $criteria = new Criteria();
     $criteria->add(CiclolectivoPeer::FK_ESTABLECIMIENTO_ID, $this->getUser()->getAttribute('fk_establecimiento_id'));
-    $cicloslectivos = CiclolectivoPeer::doSelect($criteria);                     
+    $cicloslectivos = CiclolectivoPeer::doSelect($criteria);
     $this->debugMessage(count($cicloslectivos));
-    $optionsCiclolectivo = array();                                                
-    foreach ($cicloslectivos as $ciclolectivo) {                                 
+    $optionsCiclolectivo = array();
+    foreach ($cicloslectivos as $ciclolectivo) {
         $optionsCiclolectivo[$ciclolectivo->getId()] = $ciclolectivo->getDescripcion();
-    }                                                                                 
-    asort($optionsCiclolectivo);                                                   
-    $this->optionsCiclolectivo = $optionsCiclolectivo;                          
-                                                                                                                 
-                                                                                                    
-    if ($this->getRequest()->getMethod() == sfRequest::POST)                                        
-    {                                                                                               
-      $this->feriado = $this->getFeriadoOrCreate();                                                 
-                                                                                                    
-      $this->updateFeriadoFromRequest();                                                            
-                                                                                                    
-      $this->saveFeriado($this->feriado);                                                           
-                                                                                                    
-      $this->setFlash('notice', 'Your modifications have been saved');                              
-                                                                                                    
-      if ($this->getRequestParameter('save_and_add'))                                               
-      {                                                                                             
-        return $this->redirect('feriado/create');                                                   
-      }                                                                                             
-      else                                                                                          
-      {                                                                                             
-        return $this->redirect('feriado/edit?id='.$this->feriado->getId());                         
-      }                                                                                             
-    }                                                                                               
-    else                                                                                            
-    {                                                                                               
-      // add javascripts                                                                            
-      $this->getResponse()->addJavascript(sfConfig::get('sf_prototype_web_dir').'/js/prototype');                         
-      $this->getResponse()->addJavascript(sfConfig::get('sf_admin_web_dir').'/js/collapse');                              
+    }
+    asort($optionsCiclolectivo);
+    $this->optionsCiclolectivo = $optionsCiclolectivo;
+
+    if ($this->getRequest()->getMethod() == sfRequest::POST)
+    {
+      $this->feriado = $this->getFeriadoOrCreate();
+      $this->updateFeriadoFromRequest();
+      $this->saveFeriado($this->feriado);
+      $this->setFlash('notice', 'Your modifications have been saved');
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('feriado/create');
+      }
+      else
+      {
+        return $this->redirect('feriado/edit?id='.$this->feriado->getId());
+      }
+    }
+    else
+    {
+      // add javascripts
+      $this->getResponse()->addJavascript(sfConfig::get('sf_prototype_web_dir').'/js/prototype');
+      $this->getResponse()->addJavascript(sfConfig::get('sf_admin_web_dir').'/js/collapse');
       $this->labels = $this->getLabels();
-    }                                                                                               
-  }                                                                                                 
-           
-	protected function addFiltersCriteria($c) {                                                                                    
-		$c->add(FeriadoPeer::FK_CICLOLECTIVO_ID,$this->getUser()->getAttribute('fk_ciclolectivo_id'));
-    }                                                                                                   
+    }
+  }
+
+    protected function addFiltersCriteria($c) {
+        $c->add(FeriadoPeer::FK_CICLOLECTIVO_ID,$this->getUser()->getAttribute('fk_ciclolectivo_id'));
+    }
 
 
     function saveFeriado($feriado) {
@@ -97,7 +91,38 @@ class feriadoActions extends autoferiadoActions
         $feriado->save();
     }
 
-             
+  protected function updateFeriadoFromRequest()
+  {
+    $feriado = $this->getRequestParameter('feriado');
+
+    if (isset($feriado['nombre']))
+    {
+      $this->feriado->setNombre($feriado['nombre']);
+    }
+    if (isset($feriado['fecha']))
+    {
+      if ($feriado['fecha'])
+      {
+        try
+        {
+            list($d, $m, $y) = sfI18N::getDateForCulture($feriado['fecha'], $this->getUser()->getCulture());
+            $this->feriado->setFecha("$y-$m-$d");
+        }
+        catch (sfException $e)
+        {
+          // not a date
+        }
+      }
+      else
+      {
+        $this->feriado->setFecha(null);
+      }
+    }
+    $this->feriado->setRepeticionAnual(isset($feriado['repeticion_anual']) ? $feriado['repeticion_anual'] : 0);
+    $this->feriado->setInamovible(isset($feriado['inamovible']) ? $feriado['inamovible'] : 0);
+  }
+
+
 }
 
 ?>
