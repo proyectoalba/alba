92b3fe60337ad0bf8c659fc4178b5d67c4838420
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-09-12 20:33:55 +0000

    info en el error

--- web/errors/error500.php
+++ web/errors/error500.php
@@ -1,14 +1,10 @@
 <div align="center">
-<h1>500 - Error Encontrado</h1>
-<div class="error">
-<p>M&oacute;dulo: <?echo $sf_params->get('module')?>, Acci&oacute;n: <? echo $sf_params->get('action')?></p>
-</div>
-<p>
-<br/>
-<i>Puede volver hacia atras con su navegador, consulte con el Administrador del sistema.</i>
-<br/>
-
-</p>
-
-<p><?echo link_to("Volver al Inicio","/")?></p>
+    <h1>500 - Error Encontrado</h1>
+    <div class="error">
+        <p><?php echo $exception->getMessage()?></p>
+    </div>
+    <p>
+        <i>Puede volver hacia atras con su navegador, <br/>
+        consulte con el Administrador del sistema.</i>
+    </p>
 </div>
