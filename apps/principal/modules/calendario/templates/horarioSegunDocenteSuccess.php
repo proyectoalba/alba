5acc1999b5a2017bd23580b08e973b847b6663d1
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-09-26 18:33:39 +0000

    Mejoras al codigo de los templates que llaman al componente del calendario

--- apps/principal/modules/calendario/templates/horarioSegunDocenteSuccess.php
+++ apps/principal/modules/calendario/templates/horarioSegunDocenteSuccess.php
@@ -1,10 +1,9 @@
 <h1>Horario para el Docente: <?php echo $docente->getApellido()." ".$docente->getNombre()?></h1>
 <?php
-//     include_component('icalVisualizador', $view, array('archivo' => $archivo, 'date_component' => $date_component));
     include_component('cal', $view, array(
                         'archivo' => $archivo,
                         'date' => $date_component,
-                        'verPorDia' => "view/verPorDia",
+                        'verPorDia' => 'view/verPorDia',
                         'verPorSemana' => 'view/verPorSemana',
                         'verPorMes' => 'view/verPorMes',
                         'verPorAnio' => 'view/verPorAnio'
