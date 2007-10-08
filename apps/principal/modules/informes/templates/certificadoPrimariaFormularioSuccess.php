8b477ee9b2a6ac43080a43aee4edb0140ab820e9
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2007-10-08 13:50:37 +0000

    Agregue icono nuevo para ver el informe sacado de http://www.famfamfam.com/lab/icons/silk/

--- apps/principal/modules/informes/templates/certificadoPrimariaFormularioSuccess.php
+++ apps/principal/modules/informes/templates/certificadoPrimariaFormularioSuccess.php
@@ -36,7 +36,7 @@
     <td><?php echo $alumno->alumno_apellido." ".$alumno->alumno_nombre; ?></td>
     <td>
     <ul class="sf_admin_td_actions">
-     <li><?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/edit_icon.png', array('alt' => 'editar', 'title' => 'Editar')) , 'informes?action=certificadoPrimariaListado&alumno_id='.$alumno->alumno_id); ?></li>
+     <li><?php echo link_to(image_tag('/images/small/report_go.png', array('alt' => 'editar', 'title' => 'Editar')) , 'informes?action=certificadoPrimariaListado&alumno_id='.$alumno->alumno_id); ?></li>
     </ul>
     </td>
   </tr>
--- apps/principal/modules/informes/templates/solicitudLegajoBusquedaFormularioSuccess.php
+++ apps/principal/modules/informes/templates/solicitudLegajoBusquedaFormularioSuccess.php
@@ -46,7 +46,7 @@
     <td><?php echo $alumno->alumno_apellido." ".$alumno->alumno_nombre; ?> ( <?php echo $alumno->anio_descripcion?> - <?php echo $alumno->division_nombre?> ) </td>
     <td>
     <ul class="sf_admin_td_actions">
-     <li><?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/edit_icon.png', array('alt' => 'editar', 'title' => 'Editar')) , 'informes?action=solicitudLegajoFormulario&alumno_id='.$alumno->alumno_id."&division_id=".$alumno->division_id); ?></li>
+     <li><?php echo link_to(image_tag('/images/small/report_go.png', array('alt' => 'editar', 'title' => 'Editar')) , 'informes?action=solicitudLegajoFormulario&alumno_id='.$alumno->alumno_id."&division_id=".$alumno->division_id); ?></li>
     </ul>
     </td>
   </tr>
