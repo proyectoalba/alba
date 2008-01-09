9964059893528544a131787ad09eab930d9c74f3
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2008-01-09 18:12:37 +0000

    Arreglo para que funcione el paginado en los informes

--- apps/principal/modules/informes/templates/_list.php
+++ apps/principal/modules/informes/templates/_list.php
@@ -17,15 +17,15 @@
 <tr><th colspan="8">
 <div class="float-right">
 <?php if ($pager->haveToPaginate()): ?>
-  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/first.png', array('align' => 'absmiddle', 'alt' => __('First'), 'title' => __('First'))), 'informe/list?page=1') ?>
-  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/previous.png', array('align' => 'absmiddle', 'alt' => __('Previous'), 'title' => __('Previous'))), 'informe/list?page='.$pager->getPreviousPage()) ?>
+  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/first.png', array('align' => 'absmiddle', 'alt' => __('First'), 'title' => __('First'))), 'informes/list?page=1') ?>
+  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/previous.png', array('align' => 'absmiddle', 'alt' => __('Previous'), 'title' => __('Previous'))), 'informes/list?page='.$pager->getPreviousPage()) ?>
 
   <?php foreach ($pager->getLinks() as $page): ?>
-    <?php echo link_to_unless($page == $pager->getPage(), $page, 'informe/list?page='.$page) ?>
+    <?php echo link_to_unless($page == $pager->getPage(), $page, 'informes/list?page='.$page) ?>
   <?php endforeach; ?>
 
-  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', array('align' => 'absmiddle', 'alt' => __('Next'), 'title' => __('Next'))), 'informe/list?page='.$pager->getNextPage()) ?>
-  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/last.png', array('align' => 'absmiddle', 'alt' => __('Last'), 'title' => __('Last'))), 'informe/list?page='.$pager->getLastPage()) ?>
+  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', array('align' => 'absmiddle', 'alt' => __('Next'), 'title' => __('Next'))), 'informes/list?page='.$pager->getNextPage()) ?>
+  <?php echo link_to(image_tag(sfConfig::get('sf_admin_web_dir').'/images/last.png', array('align' => 'absmiddle', 'alt' => __('Last'), 'title' => __('Last'))), 'informes/list?page='.$pager->getLastPage()) ?>
 <?php endif; ?>
 </div>
 <?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults()) ?>
