233300906b23dd749375206763083405136383ea
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2008-02-15 00:22:56 +0000

    Habilotando datos de ejemplo para pgsql

--- web/instalar/paso4.php
+++ web/instalar/paso4.php
@@ -75,13 +75,10 @@ a utilizar el sistema con una base vac&iacute;a (datos m&iacute;nimos)<br/>
             <td>Datos m&iacute;nimos:</td>
             <td><input type="radio" name="tipo_base" value="minima" <?php echo isset($_POST['tipo_base']) && $_POST['tipo_base'] == 'minima' ? 'checked' : ''?> ></td>
         </tr>
-<?php if($tipo_motor_base != 'pgsql'): //Provisoria mientras no funcione los datos de ejemplo desde el YML ?>
         <tr>
             <td>Datos ejemplo:</td>
             <td><input type="radio" name="tipo_base" value="ejemplo1" <?php echo isset($_POST['tipo_base']) && $_POST['tipo_base'] == 'ejemplo1' ? 'checked' : ''?>></td>
         </tr>
-<?php endif; ?>
-
     </table>
     <br/>
     <input type="submit" name="btEnviar" value="Seleccionar base de datos" class="boton">
