26d4a9a97232b84b2095bc67e7f64c89abfd6671
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2009-04-20 20:54:48 +0000

    eliminando archivos ignorados

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// symfony directories
-//$sf_symfony_lib_dir  = realpath(dirname(__FILE__).'/../../dist/symfony/lib');
-//$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../dist/symfony/data');
-//
-?>
