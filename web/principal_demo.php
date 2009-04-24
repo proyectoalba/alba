423bfbb6725e504c343f87bbfd202b839e3338da
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2009-04-24 18:51:34 +0000

    arreglando controladores y fix comentario en el css

--- web/principal_demo.php
+++ web/principal_demo.php
@@ -1,22 +1,12 @@
 <?php
+// this check prevents access to debug front conrollers that are deployed by accident to production servers.
+// feel free to remove this, extend it or make something more sophisticated.
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+{
+  die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
-define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
-define('SF_APP',         'principal');
-define('SF_ENVIRONMENT', 'demo');
-define('SF_DEBUG',       false);
-
-require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
-
-sfContext::getInstance()->getController()->dispatch();
-
-
-
-/*
-<?php
-##IP_CHECK##
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('principal', 'demo', false);
 sfContext::createInstance($configuration)->dispatch();
-
-*/
