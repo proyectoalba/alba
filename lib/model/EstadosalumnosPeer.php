60b28e0112c320042d56e676030c963dcabe3c49
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2009-05-15 20:49:44 +0000

    Generando modulo de estados alumnos

---
+++ lib/model/EstadosalumnosPeer.php
@@ -0,0 +1,5 @@
+<?php
+
+class EstadosalumnosPeer extends BaseEstadosalumnosPeer
+{
+}
---
+++ lib/model/om/BaseEstadosalumnos.php
@@ -0,0 +1,385 @@
+<?php
+
+
+abstract class BaseEstadosalumnos extends BaseObject  implements Persistent {
+
+
+  const PEER = 'EstadosalumnosPeer';
+
+	
+	protected static $peer;
+
+	
+	protected $id;
+
+	
+	protected $nombre;
+
+	
+	protected $alreadyInSave = false;
+
+	
+	protected $alreadyInValidation = false;
+
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->applyDefaultValues();
+	}
+
+	
+	public function applyDefaultValues()
+	{
+	}
+
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	
+	public function getNombre()
+	{
+		return $this->nombre;
+	}
+
+	
+	public function setId($v)
+	{
+		if ($v !== null) {
+			$v = (int) $v;
+		}
+
+		if ($this->id !== $v) {
+			$this->id = $v;
+			$this->modifiedColumns[] = EstadosalumnosPeer::ID;
+		}
+
+		return $this;
+	} 
+	
+	public function setNombre($v)
+	{
+		if ($v !== null) {
+			$v = (string) $v;
+		}
+
+		if ($this->nombre !== $v) {
+			$this->nombre = $v;
+			$this->modifiedColumns[] = EstadosalumnosPeer::NOMBRE;
+		}
+
+		return $this;
+	} 
+	
+	public function hasOnlyDefaultValues()
+	{
+						if (array_diff($this->modifiedColumns, array())) {
+				return false;
+			}
+
+				return true;
+	} 
+	
+	public function hydrate($row, $startcol = 0, $rehydrate = false)
+	{
+		try {
+
+			$this->id = ($row[$startcol + 0] !== null) ? (int) $row[$startcol + 0] : null;
+			$this->nombre = ($row[$startcol + 1] !== null) ? (string) $row[$startcol + 1] : null;
+			$this->resetModified();
+
+			$this->setNew(false);
+
+			if ($rehydrate) {
+				$this->ensureConsistency();
+			}
+
+						return $startcol + 2; 
+		} catch (Exception $e) {
+			throw new PropelException("Error populating Estadosalumnos object", $e);
+		}
+	}
+
+	
+	public function ensureConsistency()
+	{
+
+	} 
+	
+	public function reload($deep = false, PropelPDO $con = null)
+	{
+		if ($this->isDeleted()) {
+			throw new PropelException("Cannot reload a deleted object.");
+		}
+
+		if ($this->isNew()) {
+			throw new PropelException("Cannot reload an unsaved object.");
+		}
+
+		if ($con === null) {
+			$con = Propel::getConnection(EstadosalumnosPeer::DATABASE_NAME, Propel::CONNECTION_READ);
+		}
+
+				
+		$stmt = EstadosalumnosPeer::doSelectStmt($this->buildPkeyCriteria(), $con);
+		$row = $stmt->fetch(PDO::FETCH_NUM);
+		$stmt->closeCursor();
+		if (!$row) {
+			throw new PropelException('Cannot find matching row in the database to reload object values.');
+		}
+		$this->hydrate($row, 0, true); 
+		if ($deep) {  
+		} 	}
+
+	
+	public function delete(PropelPDO $con = null)
+	{
+		if ($this->isDeleted()) {
+			throw new PropelException("This object has already been deleted.");
+		}
+
+		if ($con === null) {
+			$con = Propel::getConnection(EstadosalumnosPeer::DATABASE_NAME, Propel::CONNECTION_WRITE);
+		}
+		
+		$con->beginTransaction();
+		try {
+			EstadosalumnosPeer::doDelete($this, $con);
+			$this->setDeleted(true);
+			$con->commit();
+		} catch (PropelException $e) {
+			$con->rollBack();
+			throw $e;
+		}
+	}
+
+	
+	public function save(PropelPDO $con = null)
+	{
+		if ($this->isDeleted()) {
+			throw new PropelException("You cannot save an object that has been deleted.");
+		}
+
+		if ($con === null) {
+			$con = Propel::getConnection(EstadosalumnosPeer::DATABASE_NAME, Propel::CONNECTION_WRITE);
+		}
+		
+		$con->beginTransaction();
+		try {
+			$affectedRows = $this->doSave($con);
+			$con->commit();
+			EstadosalumnosPeer::addInstanceToPool($this);
+			return $affectedRows;
+		} catch (PropelException $e) {
+			$con->rollBack();
+			throw $e;
+		}
+	}
+
+	
+	protected function doSave(PropelPDO $con)
+	{
+		$affectedRows = 0; 		if (!$this->alreadyInSave) {
+			$this->alreadyInSave = true;
+
+			if ($this->isNew() ) {
+				$this->modifiedColumns[] = EstadosalumnosPeer::ID;
+			}
+
+						if ($this->isModified()) {
+				if ($this->isNew()) {
+					$pk = EstadosalumnosPeer::doInsert($this, $con);
+					$affectedRows += 1; 										 										 
+					$this->setId($pk);  
+					$this->setNew(false);
+				} else {
+					$affectedRows += EstadosalumnosPeer::doUpdate($this, $con);
+				}
+
+				$this->resetModified(); 			}
+
+			$this->alreadyInSave = false;
+
+		}
+		return $affectedRows;
+	} 
+	
+	protected $validationFailures = array();
+
+	
+	public function getValidationFailures()
+	{
+		return $this->validationFailures;
+	}
+
+	
+	public function validate($columns = null)
+	{
+		$res = $this->doValidate($columns);
+		if ($res === true) {
+			$this->validationFailures = array();
+			return true;
+		} else {
+			$this->validationFailures = $res;
+			return false;
+		}
+	}
+
+	
+	protected function doValidate($columns = null)
+	{
+		if (!$this->alreadyInValidation) {
+			$this->alreadyInValidation = true;
+			$retval = null;
+
+			$failureMap = array();
+
+
+			if (($retval = EstadosalumnosPeer::doValidate($this, $columns)) !== true) {
+				$failureMap = array_merge($failureMap, $retval);
+			}
+
+
+
+			$this->alreadyInValidation = false;
+		}
+
+		return (!empty($failureMap) ? $failureMap : true);
+	}
+
+	
+	public function getByName($name, $type = BasePeer::TYPE_PHPNAME)
+	{
+		$pos = EstadosalumnosPeer::translateFieldName($name, $type, BasePeer::TYPE_NUM);
+		$field = $this->getByPosition($pos);
+		return $field;
+	}
+
+	
+	public function getByPosition($pos)
+	{
+		switch($pos) {
+			case 0:
+				return $this->getId();
+				break;
+			case 1:
+				return $this->getNombre();
+				break;
+			default:
+				return null;
+				break;
+		} 	}
+
+	
+	public function toArray($keyType = BasePeer::TYPE_PHPNAME, $includeLazyLoadColumns = true)
+	{
+		$keys = EstadosalumnosPeer::getFieldNames($keyType);
+		$result = array(
+			$keys[0] => $this->getId(),
+			$keys[1] => $this->getNombre(),
+		);
+		return $result;
+	}
+
+	
+	public function setByName($name, $value, $type = BasePeer::TYPE_PHPNAME)
+	{
+		$pos = EstadosalumnosPeer::translateFieldName($name, $type, BasePeer::TYPE_NUM);
+		return $this->setByPosition($pos, $value);
+	}
+
+	
+	public function setByPosition($pos, $value)
+	{
+		switch($pos) {
+			case 0:
+				$this->setId($value);
+				break;
+			case 1:
+				$this->setNombre($value);
+				break;
+		} 	}
+
+	
+	public function fromArray($arr, $keyType = BasePeer::TYPE_PHPNAME)
+	{
+		$keys = EstadosalumnosPeer::getFieldNames($keyType);
+
+		if (array_key_exists($keys[0], $arr)) $this->setId($arr[$keys[0]]);
+		if (array_key_exists($keys[1], $arr)) $this->setNombre($arr[$keys[1]]);
+	}
+
+	
+	public function buildCriteria()
+	{
+		$criteria = new Criteria(EstadosalumnosPeer::DATABASE_NAME);
+
+		if ($this->isColumnModified(EstadosalumnosPeer::ID)) $criteria->add(EstadosalumnosPeer::ID, $this->id);
+		if ($this->isColumnModified(EstadosalumnosPeer::NOMBRE)) $criteria->add(EstadosalumnosPeer::NOMBRE, $this->nombre);
+
+		return $criteria;
+	}
+
+	
+	public function buildPkeyCriteria()
+	{
+		$criteria = new Criteria(EstadosalumnosPeer::DATABASE_NAME);
+
+		$criteria->add(EstadosalumnosPeer::ID, $this->id);
+
+		return $criteria;
+	}
+
+	
+	public function getPrimaryKey()
+	{
+		return $this->getId();
+	}
+
+	
+	public function setPrimaryKey($key)
+	{
+		$this->setId($key);
+	}
+
+	
+	public function copyInto($copyObj, $deepCopy = false)
+	{
+
+		$copyObj->setNombre($this->nombre);
+
+
+		$copyObj->setNew(true);
+
+		$copyObj->setId(NULL); 
+	}
+
+	
+	public function copy($deepCopy = false)
+	{
+				$clazz = get_class($this);
+		$copyObj = new $clazz();
+		$this->copyInto($copyObj, $deepCopy);
+		return $copyObj;
+	}
+
+	
+	public function getPeer()
+	{
+		if (self::$peer === null) {
+			self::$peer = new EstadosalumnosPeer();
+		}
+		return self::$peer;
+	}
+
+	
+	public function clearAllReferences($deep = false)
+	{
+		if ($deep) {
+		} 
+	}
+
+} 
