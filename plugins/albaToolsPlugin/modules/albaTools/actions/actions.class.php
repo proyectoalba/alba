0838ba567b9a185cde2fd7bd578de2470fa0a388
ftoledo <ftoledo@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2009-06-01 21:59:00 +0000

    nuevo plugin de alba para con un par de herramientas

--- plugins/albaToolsPlugin/modules/albaTools/actions/actions.class.php
+++ plugins/albaToolsPlugin/modules/albaTools/actions/actions.class.php
@@ -1,13 +1,41 @@
 <?php
 class albaToolsActions extends sfActions
 {
-    public function executeConvert()
+
+    /**
+    * Convierte un texto a imagen
+    * @param string $texto
+    */
+    public function executeText2img(sfWebRequest $request)
     {
-        return $this->renderText("hola");
+    	//fuente por defecto
+    	$this->font = 2;
+    	//obtengo el texto
+    	$texto = $request->getParameter('texto');
+		//seteo el header
+        $this->setLayout(false);
+        $this->getResponse()->clearHttpHeaders();
+        $this->getResponse()->setContentType("Content-type: image/png");
+
+		// calculo el alto
+		$alto = strlen($texto) * imagefontwidth($this->font) + imagefontwidth($this->font);
+
+		$img_handle = imagecreatetruecolor (imagefontheight($this->font), $alto) or die ("Cannot Create image");
+
+		// ImageColorAllocate (image, red, green, blue)
+		$back_color = ImageColorAllocate ($img_handle, 255, 255,255 );
+		$txt_color = ImageColorAllocate ($img_handle, 0, 0, 0);
+		ImageFill($img_handle,0,0, $back_color);
+		ImageStringUp ($img_handle, $this->font, 0,$alto  - (imagefontwidth($this->font)/2), $texto, $txt_color);
+		ImagePng ($img_handle);
+		ImageDestroy($img_handle);
+		return sfView::NONE;
     }
+
+    /* demostracion delas utilidades */
     public function executeIndex()
     {
-    
+
     }
 
 }
---
+++ plugins/albaToolsPlugin/modules/albaTools/templates/indexSuccess.php
@@ -0,0 +1,5 @@
+<?php use_helper('AlbaTools')?>
+<h1>Herramientas para Alba</h1>
+
+<p>Imagen vertical</p>
+<?php echo text2img('hola')?>
