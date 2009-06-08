173065e9e0f008eb77ad3d006900bf5a495e7022
josx <josx@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2009-06-08 20:10:09 +0000

    Agregando salto de linea para el listado de boletines

--- apps/principal/modules/informes/templates/boletinListadoSuccess.php
+++ apps/principal/modules/informes/templates/boletinListadoSuccess.php
@@ -1,5 +1,12 @@
 <?php
+    $i = 0;
     foreach($checks as $alumno_id => $division_id) { 
+        $h = ++$i%2;
         include_component('boletin','mostrar', array( 'alumno_id' => $alumno_id, 'division_id' => $division_id));
+        if($h == 0) {
+?>
+        <div class="pageBreak" />
+<?
+        }
     }
 ?>
