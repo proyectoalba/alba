43063c02b86d9714e0c1a8b9fbfbbfe2218dc95e
hsanchez <hsanchez@9f9aa102-def4-0310-94d0-87cd3e302e5b>
2011-07-20 22:50:10 +0000

    correccion de ortografia

--- lib/formulas/formula_Promedio.class.php
+++ lib/formulas/formula_Promedio.class.php
@@ -6,7 +6,7 @@ class formula_Promedio extends albaFormulaBoletin {
 
  public function init()
   {
-    $this->ayuda = "Esta formula calcula el promedio entre las notas de 3 peridoso p1,p2 y p3\n";
+    $this->ayuda = "Esta formula calcula el promedio entre las notas de 3 periodos p1,p2 y p3\n";
     $this->ayuda .= "Se debe utilizar: Promedio|4,5,6\n";
     $this->ayuda .= "Donde 4,5 y 6 son los ID's de los periodos que se obtienen las notas";
     $this->nombre = 'Promedio';
