d5ea140b98778c5ef73a6a4b0921b0960c32e68f
ftoledo <ftoledo@docksud.com.ar>
2012-09-21 18:55:38 -0300

    correcion de upgrade de 1.0 a 1.1, tabla nueva de config

--- data/sql/db-1.1-1.2.sql
+++ data/sql/db-1.1-1.2.sql
@@ -1,3 +1,12 @@
 ALTER TABLE alumno ADD COLUMN `email_padre` VARCHAR(128);
 ALTER TABLE alumno ADD COLUMN  `celular_padre`  VARCHAR(20);
 ALTER TABLE alumno ADD COLUMN  `celular_madre` VARCHAR(20);
+
+CREATE TABLE config (
+  id int auto_increment not null,
+  nombre varchar(200),
+  valor text,
+  primary key (id)
+);
+
+INSERT INTO config (nombre,valor) VALUES ('db_version','1.2');
